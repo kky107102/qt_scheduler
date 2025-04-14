189c1d1ed769e7d8cc819b7af14b2195eb7ac6cb
yjseo01 <yujin071610@gmail.com>
2025-04-14 19:21:56 +0900

    feat: calendarWidget 함수 추가 구현

--- calenderwidget.cpp
+++ calenderwidget.cpp
@@ -6,9 +6,62 @@ calenderWidget::calenderWidget(QWidget *parent)
     , ui(new Ui::calenderWidget)
 {
     ui->setupUi(this);
+    ui->calendarWidget->showToday();
+    getSchedules();
+    paintSchedules();
 }
 
 calenderWidget::~calenderWidget()
 {
     delete ui;
+}
+
+bool calenderWidget::getSchedules()
+{
+    int year = ui->calendarWidget->yearShown();
+    int month = ui->calendarWidget->monthShown();
+    QDate firstDate(year, month, 1);
+    QTime startOfDay(0, 0, 0);
+    QDateTime startDateTime(firstDate, startOfDay);
+
+    QDate lastDate = firstDate.addMonths(1).addDays(-1);
+    QTime endOfDay(23, 59, 59);
+    QDateTime endDateTime(lastDate, endOfDay);
+
+    qDebug() << "startDate: " << startDateTime.toString(Qt::ISODate);
+    qDebug() << "endDate:" << endDateTime.toString(Qt::ISODate);
+
+    QList<Schedule> sList = dbManager::instance().searchSchedule(startDateTime, endDateTime);
+    for (const Schedule& s : sList)
+    {
+        schedules.append(s);
+        qDebug() << s.getScheduleId() << " "
+                << " " << s.getStartTime() << " " << s.getEndTime();
+    }
+
+    return true;
+}
+
+void calenderWidget::paintSchedules()
+{
+    qDebug() << "paintSchedules() called";
+
+    QTextCharFormat highlightFormat;
+    highlightFormat.setBackground(Qt::blue);
+
+    for (const Schedule& s : schedules)
+    {
+        QDate sDate = s.getStartTime().date();
+        QDate eDate = s.getEndTime().date();
+
+        for (QDate d = sDate; d <= eDate; d = d.addDays(1))
+            ui->calendarWidget->setDateTextFormat(d, highlightFormat);
+    }
+
+}
+
+// slots
+void calenderWidget::onClickedDate()
+{
+    qDebug() << "onClickedDate()";
 }
